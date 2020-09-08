f45ab92265f09d1ecd9b89603181971e098bbb84
pd910900 <pd910900@posco.com>
2020-09-08 16:29:17 +0900

    Updates

---
+++ precache-manifest.4015700ccb0e6521e3dd3350a5f0e471.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d42c5e08cd80c74ba10585862b23aea",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "fdb8d2df6b66f5e4e5a5",
+    "url": "/portfolio/static/css/main.2c7a68b5.chunk.css"
+  },
+  {
+    "revision": "532530016d76d142830f",
+    "url": "/portfolio/static/js/2.a72ff635.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portfolio/static/js/2.a72ff635.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fdb8d2df6b66f5e4e5a5",
+    "url": "/portfolio/static/js/main.455a2c06.chunk.js"
+  },
+  {
+    "revision": "e222e433a781ad285fbc",
+    "url": "/portfolio/static/js/runtime-main.15f80a31.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/portfolio/static/media/logo.ee7cd8ed.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.8312f3744c3884b6f4dfa20e5eb96c82.js"
+  "/portfolio/precache-manifest.4015700ccb0e6521e3dd3350a5f0e471.js"
 );
 
 self.addEventListener('message', (event) => {
