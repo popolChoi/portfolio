3e5baf4e06e73ffbe8e3772b031b4a3f2c3e70e6
pd910900 <pd910900@posco.com>
2020-09-11 15:46:56 +0900

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.3ed0ec65768bc42278a6f7c87c3a1e84.js"
+  "/portfolio/precache-manifest.fac638e5ccac1713e53de70b311a4ee5.js"
 );
 
 self.addEventListener('message', (event) => {
